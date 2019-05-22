e8abd539af2419251cf880a925aa64e891c56453
Christopher Cantrell <topherCantrell@gmail.com>
2019-05-22 13:05:20 -0500

    starting to find/make moves

--- src/app_farm.py
+++ src/app_farm.py
@@ -2,10 +2,24 @@
 import board as BOARD
 import cards as CARDS
 
+# Play several games (checking for exceptions)
+for _ in range(1000):
 
-deck = CARDS.make_deck()
-CARDS.shuffle_deck(deck)
+    deck = CARDS.make_deck()
+    CARDS.shuffle_deck(deck)
 
-board = BOARD.new_board(deck)
+    board = BOARD.new_board(deck)
+    BOARD.show_board(board)
 
-BOARD.show_board(board)
+    for m in range(100):
+        moves = BOARD.find_moves(board)
+        print(moves)
+        if not moves:
+            break
+        BOARD.make_move(moves[0], board)
+        BOARD.show_board(board)
+
+    score = BOARD.get_score(board)
+    print('Score', score)
+    if score > 5:
+        raise Exception('WOO HOO')
--- src/board.py
+++ src/board.py
@@ -5,11 +5,11 @@ Terminology:
   Foundation - 4 columns of cards (the aces)
   Pile       - cards flipped 3 at a time
   Stacks     - 7 columns of cards
-  
+
 Targets:
 
-The last card in each of the 4 foundation sets is accessible. Cards can be removed from
-the end or added to the end. The targets are named 'Fh', 'Fd', 'FC', 'FS'.
+The last card in each of the 4 foundation sets is accessible. Moving cards to the foundation, the
+target 'F' is used. Moving cards out of the foundation requires 'Fh','Fd', 'FC', or 'FS'.
 
 The last card if each of the 7 stacks is accessible. The first card turned over in a stack
 is accessible (the rest of the stack moves with it). The stacks are named 'S0', 'S1', 'S2',
@@ -39,6 +39,97 @@ Game facts used to weigh moves:
 '''
 
 import cards as CARDS
+
+FOUNDATION_SUIT_ORDER = ['hearts', 'diamonds', 'clubs', 'spades']
+
+
+def get_score(board):
+    cnt = 0
+    for i in range(4):
+        cnt = cnt + len(board['foundation'][i])
+    return cnt
+
+
+def _does_fit(src_card, dst_card, is_stacks):
+    # example card: [2, '2'], ['clubs', 'C', 'black']
+    if is_stacks:
+        # Special case for Aces ... no need to move them around the stacks
+        if src_card[0][0] == 1:
+            return False
+        # Red/Black alternating
+        if src_card[1][2] == dst_card[1][2]:
+            return False
+        # SRC value must be DST+1
+        if src_card[0][0] == dst_card[0][0] - 1:
+            return True
+        return False
+    else:
+        # Must be the foundation. Suits must match.
+        if src_card[1][0] != dst_card[1][0]:
+            return False
+        # SRC value must be DST+1
+        if src_card[0][0] == dst_card[0][0] + 1:
+            return True
+        return False
+
+
+def find_moves(board):
+    ret = []
+
+    # Moving cards from the ends of the stacks to the foundation
+    for src in range(7):
+        if not board['stacks'][src]:
+            continue
+        card = board['stacks'][src][-1]
+        fid = FOUNDATION_SUIT_ORDER.index(card[1][0])
+
+        if board['foundation'][fid]:
+            # Stack on previous
+            if _does_fit(card, board['foundation'][fid][-1], False):
+                ret.append('S{}e-F'.format(src, card[1][1]))
+        else:
+            # Aces to empty
+            if card[0][0] == 1:
+                ret.append('S{}e-F'.format(src))
+
+    # Moving cards around the ends of the stacks
+    for src in range(7):
+        if not board['stacks'][src]:
+            continue
+        for dst in range(7):
+            if src == dst:
+                continue
+            if not board['stacks'][dst]:
+                continue
+            if _does_fit(board['stacks'][src][-1], board['stacks'][dst][-1], True):
+                ret.append('S{}e-S{}e'.format(src, dst))
+
+    return ret
+
+
+def make_move(move, board):
+
+    # Moving cards from the ends of the stacks to the foundation
+    if move[0] == 'S' and move.endswith('e-F'):
+        src = int(move[1])
+        card = board['stacks'][src].pop()
+        if len(board['stacks'][src]) <= board['stacks_pos'][src]:
+            board['stacks_pos'][src] -= 1
+        board['foundation'][FOUNDATION_SUIT_ORDER.index(
+            card[1][0])].append(card)
+        return
+
+    # Moving cards around the ends of the stacks
+    if move[0] == 'S' and move[-1] == 'e' and move[2:5] == 'e-S':
+        src = int(move[1])
+        dst = int(move[5])
+        card = board['stacks'][src].pop()
+        board['stacks'][dst].append(card)
+        if len(board['stacks'][src]) <= board['stacks_pos'][src]:
+            board['stacks_pos'][src] -= 1
+        return
+
+    raise Exception('Unimplemented move ' + move)
 
 
 def new_board(deck):
