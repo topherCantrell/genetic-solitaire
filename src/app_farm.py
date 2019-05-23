6667e3ef5cb4186b5db1c3b9419397ef92942786
Christopher Cantrell <topherCantrell@gmail.com>
2019-05-23 08:49:15 -0500

    CLI

--- src/app_farm.py
+++ src/app_farm.py
@@ -16,24 +16,54 @@ Game facts used to weigh moves:
   - Number of kings in the top of the stacks
 '''
 
-# Play several games (checking for exceptions)
-for _ in range(1000):
 
-    deck = CARDS.make_deck()
-    CARDS.shuffle_deck(deck)
+def play_auto(num_games):
+    for _ in range(num_games):
 
-    board = BOARD.new_board(deck)
-    BOARD.show_board(board)
+        deck = CARDS.make_deck()
+        CARDS.shuffle_deck(deck)
 
-    for m in range(100):
-        moves = MOVES.find_moves(board)
-        print(moves)
-        if not moves:
-            break
-        MOVES.make_move(moves[0], board)
+        board = BOARD.new_board(deck)
         BOARD.show_board(board)
 
-    score = BOARD.get_score(board)
-    print('Score', score)
-    if score > 7:
-        raise Exception('WOO HOO')
+        for m in range(100):  # Limit to 100 moves
+            moves = MOVES.find_moves(board)
+            print(moves)
+            if not moves:
+                break
+            MOVES.make_move(moves[0], board)
+            BOARD.show_board(board)
+
+        score = BOARD.get_score(board)
+        print('Score', score)
+
+
+def play_cli():
+    deck = CARDS.make_deck()
+    CARDS.shuffle_deck(deck)
+    board = BOARD.new_board(deck)
+
+    while True:
+        moves = MOVES.find_moves(board)
+        BOARD.show_board(board)
+        print('------------')
+
+        if not moves:
+            print('No moves. Game over.')
+            break
+
+        for i in range(len(moves)):
+            print('{}: {}'.format(i + 1, moves[i]))
+
+        print()
+        g = input('Move: ')
+
+        MOVES.make_move(moves[int(g) - 1], board)
+        print()
+
+
+if __name__ == '__main__':
+
+    # play_auto(1000)
+
+    play_cli()
